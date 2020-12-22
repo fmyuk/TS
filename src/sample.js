137ecdc5bfe04b370a147e0772aa921ae9515f44
fumi uk <dcfumiya.238@gmail.com>
2020-12-23 08:40:35 +0900

    ts

--- src/sample.js
+++ src/sample.js
@@ -2,3 +2,21 @@ export let sampleText = "sampleText";
 export function sampleFunction() {
   return true;
 }
+
+function expo2(amount) {
+  return amount ** 2;
+}
+const value = expo2("1,000");
+const withTax = value * 1.1;
+
+
+function taxed(amount) {
+  return amount * 1.1;
+}
+function fee(amount) {
+  return `{amount * 1.4}`;
+}
+function price(amount) {
+  return `${fee(amount)}`;
+}
+const demand = "¥" + taxed(price(1000));
